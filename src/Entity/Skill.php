28328ddcc5f00dc0540249122201153b728abf2e
boatware <thwohmas@gmail.com>
2022-08-15 17:17:31 +0200

    Created Skill

---
+++ src/Entity/Skill.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SkillRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SkillRepository::class)]
+class Skill
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $level = null;
+
+    #[ORM\Column]
+    private ?int $years = null;
+
+    #[ORM\ManyToOne(inversedBy: 'skills')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?JobProfile $jobProfile = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getLevel(): ?string
+    {
+        return $this->level;
+    }
+
+    public function setLevel(string $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getYears(): ?int
+    {
+        return $this->years;
+    }
+
+    public function setYears(int $years): self
+    {
+        $this->years = $years;
+
+        return $this;
+    }
+
+    public function getJobProfile(): ?JobProfile
+    {
+        return $this->jobProfile;
+    }
+
+    public function setJobProfile(?JobProfile $jobProfile): self
+    {
+        $this->jobProfile = $jobProfile;
+
+        return $this;
+    }
+}
