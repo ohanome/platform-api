cb4761a045eb07834a86e464b79465014d3bce7a
boatware <thwohmas@gmail.com>
2022-08-15 17:55:32 +0200

    Make sure to only allow 1 and -1 as values

--- src/Entity/Like.php
+++ src/Entity/Like.php
@@ -60,6 +60,13 @@ class Like
 
     public function setValue(int $value): self
     {
+        // Make sure to only allow 1 and -1 as values.
+        if ($value >= 0) {
+            $value = 1;
+        } else {
+            $value = -1;
+        }
+
         $this->value = $value;
 
         return $this;
